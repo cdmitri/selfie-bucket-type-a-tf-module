e834a4edd04f453f026260140cc2f1d7685e1744
Dmitri Chtchourov <dimocracy@gmail.com>
2023-05-11 13:50:47 -0700

    Added contact label inside module

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "name" {
 }
 
 resource "google_storage_bucket" "bucket_type_a_1" {
-  name                        = var.name
+  name                        = "${var.name}-inside-module-lable-01"
   location                    = "us-central1"
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
